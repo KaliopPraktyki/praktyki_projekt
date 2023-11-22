4424bd9a4c70b36c18dc546aa0bd70a8350afaa0
krystian <kryroz21@gmail.com>
2023-11-22 11:24:06 +0100

    basic chat

--- lib/screens/conversation.dart
+++ lib/screens/conversation.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 
 class conversation extends StatefulWidget {
   const conversation({super.key});
@@ -8,8 +10,33 @@ class conversation extends StatefulWidget {
 }
 
 class _conversationState extends State<conversation> {
+
+  List<types.Message> _messages = [];
+  final _user = const types.User(
+    id: '82091008-a484-4a89-ae75-a22bf8d6f3ac',
+  );
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Chat(messages: _messages, onSendPressed: _handleSendPressed, user: _user),
+    );
+  }
+
+  void _handleSendPressed(types.PartialText message) {
+    final textMessage = types.TextMessage(
+      author: _user,
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      id: "id",
+      text: message.text,
+    );
+
+    _addMessage(textMessage);
+  }
+
+  void _addMessage(types.Message message) {
+    setState(() {
+      _messages.insert(0, message);
+    });
   }
 }
