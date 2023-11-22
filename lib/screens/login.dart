7a021f5f927ef5fe2d68ccc8c82046a41cfbda71
hoccuspoccus34 <greentx@wp.pl>
2023-11-22 10:12:54 +0100

    login screen added

---
+++ lib/screens/login.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class loginScreen extends StatelessWidget {
+  const loginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
