eff09859b7fcb1e1c9f7495e61c4dd05a32c4ebd
Mykola <terianyk.mykola@drzewniak.slupsk.pl>
2023-11-22 10:13:17 +0100

    update main.dart

---
+++ lib/screens/chat.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class chat extends StatefulWidget {
+  const chat({super.key});
+
+  @override
+  State<chat> createState() => _chatState();
+}
+
+class _chatState extends State<chat> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
