1cddf2e0b94809ad0d1971f035edc6d0d1ce59b2
hoccuspoccus34 <greentx@wp.pl>
2023-11-22 11:02:25 +0100

    + logo without background + fonts + update login

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -7,11 +7,42 @@ class loginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            //logo
+        child: Center(
+          child: Column(
+            children: [
+              //logo
+              SizedBox(height: 40,),
+              Image.asset('assets/logoWithoutBackground.png',
+              scale: 2.2,),
 
-          ],
+              SizedBox(height: 20,),
+
+              //Text Login
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(width: 20,),
+                  Text('Login',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                  ),),
+                ],
+              ),
+              //Please sign in to continue
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(width: 20,),
+                  Text('Please sign in to continue',
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.grey,
+                    ),),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
