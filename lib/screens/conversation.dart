e17e9fd74f6a5c92116c59f4d148aae98403434f
krystian <kryroz21@gmail.com>
2023-11-22 11:00:53 +0100

    add dependencies

--- lib/screens/conversation.dart
+++ lib/screens/conversation.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
 
+class conversation extends StatefulWidget {
+  const conversation({super.key});
+
+  @override
+  State<conversation> createState() => _conversationState();
+}
+
+class _conversationState extends State<conversation> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
