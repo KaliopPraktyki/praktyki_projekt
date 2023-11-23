acb52103ef72947f05739be7a1a1968ed8b33855
hoccuspoccus34 <greentx@wp.pl>
2023-11-23 12:46:30 +0100

    change screens login & register

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:praktyki_projekt/screens/main_screen.dart';
+import 'package:praktyki_projekt/auth/main_screen.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
