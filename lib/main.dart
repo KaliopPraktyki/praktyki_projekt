1acbd4124493bc2554285b0e82a5bbe6ab4f9453
hoccuspoccus34 <greentx@wp.pl>
2023-11-23 12:11:24 +0100

    login screen & register screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:praktyki_projekt/screens/main_screen.dart';
+import 'package:praktyki_projekt/screens/register.dart';
+import 'firebase_options.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:praktyki_projekt/screens/chat.dart';
 import 'package:praktyki_projekt/screens/login.dart';
 import 'package:praktyki_projekt/screens/conversation.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 
-void main() async {
-
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const MyApp());
 }
@@ -29,7 +34,7 @@ class MyApp extends StatelessWidget {
 
       ),
       debugShowCheckedModeBanner: false,
-      home: loginScreen(),
+      home: mainScreen(),
 
     );
   }
