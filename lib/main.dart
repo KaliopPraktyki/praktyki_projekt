1bffe6376f23436019e6db14815d36be3abbc0b9
hoccuspoccus34 <greentx@wp.pl>
2023-11-23 09:18:49 +0100

    fixes

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,7 @@ import 'package:firebase_core/firebase_core.dart';
 
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
 
   runApp(const MyApp());
 }
