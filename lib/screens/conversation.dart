0dbf10c8893f42ff0568480bf59a8f15406f341f
krystian <kryroz21@gmail.com>
2023-11-23 12:57:18 +0100

    update conversation

--- lib/screens/conversation.dart
+++ lib/screens/conversation.dart
@@ -1,42 +1,39 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
-import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+import 'package:praktyki_projekt/screens/chat.dart';
 
-class conversation extends StatefulWidget {
+class conversation extends StatelessWidget {
   const conversation({super.key});
-
-  @override
-  State<conversation> createState() => _conversationState();
-}
-
-class _conversationState extends State<conversation> {
-
-  List<types.Message> _messages = [];
-  final _user = const types.User(
-    id: '82091008-a484-4a89-ae75-a22bf8d6f3ac',
-  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Chat(messages: _messages, onSendPressed: _handleSendPressed, user: _user),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 32, 32, 33),
+        automaticallyImplyLeading: false,
+        title: Row(
+          children: [
+            BackButton(
+              color: Colors.white,
+              onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const chat()));
+              },),
+            CircleAvatar(
+            ),
+            SizedBox(width: 15),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Name", style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+                Text("online", style: TextStyle(fontSize: 12, color: Colors.white),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
-  }
-
-  void _handleSendPressed(types.PartialText message) {
-    final textMessage = types.TextMessage(
-      author: _user,
-      createdAt: DateTime.now().millisecondsSinceEpoch,
-      id: "id",
-      text: message.text,
-    );
-
-    _addMessage(textMessage);
-  }
-
-  void _addMessage(types.Message message) {
-    setState(() {
-      _messages.insert(0, message);
-    });
   }
 }
