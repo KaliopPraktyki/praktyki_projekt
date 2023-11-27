44773d092a70d12c63a2378b23a9369fa90c419c
Mykola <terianyk.mykola@drzewniak.slupsk.pl>
2023-11-27 12:36:36 +0100

    fix

--- lib/model/message.dart
+++ lib/model/message.dart
@@ -2,14 +2,14 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Message {
   final String senderId;
-  final String senderName;
+  final String senderEmail;
   final String receiverId;
   final String message;
   final Timestamp timestamp;
 
   Message({
     required this.senderId,
-    required this.senderName,
+    required this.senderEmail,
     required this.receiverId,
     required this.message,
     required this.timestamp,
@@ -18,7 +18,7 @@ class Message {
   Map<String, dynamic> toMap() {
     return {
       'senderId': senderId,
-      'senderName': senderName,
+      'senderEmail': senderEmail,
       'receiverId': receiverId,
       'message': message,
       'timestamp': timestamp,
