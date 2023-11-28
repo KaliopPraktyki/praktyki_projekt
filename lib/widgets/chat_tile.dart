584c2633da3a2a0d343b549b2af4572c0d299679
Mykola <terianyk.mykola@drzewniak.slupsk.pl>
2023-11-28 13:08:40 +0100

    fix

--- lib/widgets/chat_tile.dart
+++ lib/widgets/chat_tile.dart
@@ -28,6 +28,7 @@ class _ChatTileState extends State<ChatTile> {
           }
 
           return ListView(
+            shrinkWrap: true,
             children:
             snapshot.data!.docs
                 .map<Widget>((doc) => _buildUserListItem(doc))
@@ -44,98 +45,86 @@ class _ChatTileState extends State<ChatTile> {
     double height = MediaQuery.of(context).size.height;
 
     if(_auth.currentUser!.email != data['email']){
-      return Container(
+      return GestureDetector(
+        child: Row(
+          children: [
+            SizedBox(height: height*0.12,),
 
-        child: GestureDetector(
-          child: Row(
-            children: [
-              SizedBox(height: height*0.12,),
+            ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: Image.asset("assets/logo.png",
+                  width: 70,
+                  height: 70,)
+            ),
 
-              ClipRRect(
-                  borderRadius: BorderRadius.circular(50),
-                  child: Image.asset("assets/logo.png",
-                    width: 70,
-                    height: 70,)
-              ),
+            const SizedBox(width: 10),
 
-              const SizedBox(width: 10),
-
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(data['email'],
-                    style:const TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                    ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(data['firstName']+" "+ data['lastName'],
+                  style:const TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
                   ),
+                ),
 
-                  const SizedBox(height: 10,),
-                  Text("last message",
-                    style:const TextStyle(
+                const SizedBox(height: 10,),
+                Text("last message",
+                  style:const TextStyle(
+                    color: Colors.grey,
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
+
+            const Spacer(),
+
+            Column(
+
+              children: [
+
+                Text(
+                  "time",
+                  style: const TextStyle(
                       color: Colors.grey,
-                      fontSize: 20,
-                    ),
+                      fontSize: 20
                   ),
-                ],
-              ),
+                ),
 
-              const Spacer(),
+                const SizedBox(width: 20,height: 10,),
 
-              Column(
-
-                children: [
-
-                  Text(
-                    "time",
-                    style: const TextStyle(
-                        color: Colors.grey,
-                        fontSize: 20
-                    ),
+                Container(
+                  width: 20,
+                  height: 20,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: Color.fromARGB(255, 55, 86, 56)
                   ),
 
-                  const SizedBox(width: 20,height: 10,),
-
-                  Container(
-                    width: 20,
-                    height: 20,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(50),
-                        color: Color.fromARGB(255, 55, 86, 56)
-                    ),
-
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text("numberOffUnreadMessages",
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(
-                          fontSize: 15,
-                          color: Colors.white,
-                        ),
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text("numberOffUnreadMessages",
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
                       ),
                     ),
                   ),
-                ],
-              ),
-            ],
-          ),
-          onTap: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context)=> conversation(
-              receiverUserEmail: data['email'],
-              receiverUserID: data['userId'],
-            )));
-          },
+                ),
+              ],
+            ),
+          ],
         ),
+        onTap: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context)=> conversation(
+            receiverUserName: data['firstName']+" "+ data['lastName'],
+            receiverUserID: data['userId'],
+          )));
+        },
       );
-      // ListTile(
-      //   title: Text(data['email']),
-      //   onTap: (){
-      //     Navigator.push(context, MaterialPageRoute(builder: (context)=> conversation(
-      //       receiverUserEmail: data['email'],
-      //       receiverUserID: data['uid'],
-      //     )));
-      //   },
-      // );
     }
     else{
       return Container();
