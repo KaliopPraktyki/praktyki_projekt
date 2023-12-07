4c01ed4c5a0646c422f4106ad516fa750db17c0c
hoccuspoccus34 <greentx@wp.pl>
2023-12-07 11:18:06 +0100

    add data to firestorage and add data to user

---
+++ lib/resources/add_data.dart
@@ -0,0 +1,40 @@
+
+import 'dart:typed_data';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+
+final FirebaseStorage _storage = FirebaseStorage.instance;
+final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+final FirebaseAuth _auth = FirebaseAuth.instance;
+
+class StorageData{
+
+  Future<String> uploadImageToStorage(String childName, Uint8List file) async{
+
+    Reference ref = _storage.ref().child(childName);
+    UploadTask uploadTask = ref.putData(file);
+      TaskSnapshot snapshot = await uploadTask;
+    String downloadUrl = await snapshot.ref.getDownloadURL();
+    return downloadUrl;
+  }
+
+  Future<String> saveData({
+    required Uint8List file
+  }) async{
+    String resp = "Some error occurred";
+    try{
+     String imageUrl = await uploadImageToStorage("ProfileImage", file);
+     await _firestore.collection("users").doc(_auth.currentUser?.uid).update({
+       "profilePicture": imageUrl,
+      });
+     resp = "success";
+    }
+        catch(err){
+          resp = err.toString();
+        }
+        return resp;
+  }
+}
